e01ab747b3cb527442171965eb124415ed02affa
flesheatindragon <futuregamesstore@gmail.com>
2023-09-04 12:00:56 -0500

    Added 3 more songses >:^DDDD

--- index.php
+++ index.php
@@ -13,6 +13,15 @@
                     echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/O4irXQhgMqg?si=6kZUKpy657gica9X&amp;controls=0&amp;clip=UgkxQi4qu3zGbwmqU5mvIsKOTBRAh_712zo_&amp;clipt=EAAYjjg&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><br>';
                 }
                 if($num == 2){
+                    echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/wp43OdtAAkM?si=te0_qMAKLGgUKIDg&amp;controls=0&amp;clip=UgkxzWL-SsX2m8vhPIxaAIuLOqSHTTu0jQXD&amp;clipt=ELhJGJh1&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><br>';
+                }
+                if($num == 3){
+                    echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/FSjZ7dZVi90?si=UwaLPwBGLrGXA3Ob&amp;controls=0&amp;clip=UgkxPifXYhXAKFv2ABu6kU74BPLSoCNTQmxD&amp;clipt=EAAYjjg&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><br>';
+                }
+                if($num == 4){
+                    echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/AnNKsk2a0fI?si=Fz7OM-1pQWLzd6Fs&amp;clip=UgkxAspZ7gyudtiRSMYFEZOPzgpaa-QPDtg2&amp;clipt=EAAYsSs&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><br>';
+                }
+                if($num == 5){
                     echo '<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=1VYSHY-twD7LeImT&amp;controls=0&amp;clip=Ugkxk4ujU4eKKCJGI73BO64OzCFmUD95HyI0&amp;clipt=EAAYtjk&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><br>';
                 }
             ?>
